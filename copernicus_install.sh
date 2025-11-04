8adbbf8e3d83c3dbd607817e3d51972c75f669b6
VAN'T KLOOSTER Jurriaan <jurriaan.van't-klooster@ext.ec.europa.eu>
2025-11-04 10:24:42 +0100

    added copernicus_install.sh

---
+++ copernicus_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+target="copernicus"
+down=0
+
+# Check if any arguments were provided
+if [ $# -eq 0 ]; then
+  echo "No arguments provided."
+fi
+
+# Iterate over the arguments
+for arg in "$@"; do
+  # Check if the current argument has the value "down"
+  if [ "$arg" == "down" ]; then
+    down=1
+  fi
+done
+
+if [ $down == 1 ]; then
+  ./cs_install.sh down
+else
+  all_args="$@ -t $target"
+  ./cs_install.sh $all_args
+fi
+
